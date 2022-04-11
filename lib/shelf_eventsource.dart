0518db9f68349e86a976a7f6e990170ca379e66b
Toshi Ossada <toshiossada@gmail.com>
2022-04-11 15:36:40 -0300

    updating to new dart

--- lib/shelf_eventsource.dart
+++ lib/shelf_eventsource.dart
@@ -5,24 +5,22 @@ import "dart:convert";
 import "package:eventsource/publisher.dart";
 import "package:eventsource/src/encoder.dart";
 import "package:shelf/shelf.dart";
-import 'package:stream_channel/stream_channel.dart';
 
 /// Create a shelf handler for the specified channel.
 /// This handler can be passed to the [shelf.serve] method.
-Handler eventSourceHandler(EventSourcePublisher publisher,
-    {String channel: "", bool gzip: false}) {
+Handler eventSourceHandler(
+  EventSourcePublisher publisher, {
+  String channel = "",
+  bool gzip = false,
+}) {
   // define the handler
   Response shelfHandler(Request request) {
     if (request.method != "GET") {
-      return new Response.notFound(null);
-    }
-
-    if (request.headers["Accept"] != "text/event-stream") {
-      return new Response(400, body: "Bad Request");
+      return Response.notFound(null);
     }
 
     if (!request.canHijack) {
-      throw new ArgumentError("eventSourceHandler may only be used with a "
+      throw ArgumentError("eventSourceHandler may only be used with a "
           "server that supports request hijacking.");
     }
 
@@ -32,10 +30,9 @@ Handler eventSourceHandler(EventSourcePublisher publisher,
 
     // hijack the raw underlying channel
     request.hijack((untypedChannel) {
-      var socketChannel =
-          (untypedChannel as StreamChannel).cast/*<List<int>>*/();
+      var socketChannel = (untypedChannel).cast<List<int>>();
       // create a regular UTF8 sink to write headers
-      var sink = UTF8.encoder.startChunkedConversion(socketChannel.sink);
+      var sink = utf8.encoder.startChunkedConversion(socketChannel.sink);
       // write headers
       sink.add("HTTP/1.1 200 OK\r\n"
           "Content-Type: text/event-stream; charset=utf-8\r\n"
@@ -45,9 +42,9 @@ Handler eventSourceHandler(EventSourcePublisher publisher,
       sink.add("\r\n");
 
       // create encoder for this connection
-      var encodedSink = new EventSourceEncoder(compressed: useGzip)
+      var encodedSink = EventSourceEncoder(compressed: useGzip)
           .startChunkedConversion(socketChannel.sink);
-      
+
       // initialize the new subscription
       publisher.newSubscription(
           onEvent: encodedSink.add,
@@ -55,11 +52,6 @@ Handler eventSourceHandler(EventSourcePublisher publisher,
           channel: channel,
           lastEventId: request.headers["Last-Event-ID"]);
     });
-
-    // [request.hijack] is guaranteed to throw a [HijackException], so we'll
-    // never get here.
-    assert(false);
-    return null;
   }
 
   return shelfHandler;
