c3b50f7532518d4723433edccf81de3def37dc6d
Toshi Ossada <toshiossada@gmail.com>
2022-04-28 14:53:20 -0300

    example

--- example/shelf_eventsource_example.dart
+++ example/shelf_eventsource_example.dart
@@ -3,32 +3,48 @@ import 'dart:convert';
 
 import 'package:eventsource/publisher.dart';
 import "package:shelf/shelf_io.dart" as io;
-import "package:shelf_eventsource/shelf_eventsource.dart";
+import 'package:shelf_eventsource/shelf_eventsource.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 main() {
   var app = Router();
-  app.get("/events", (r) {
-    final publisher = EventSourcePublisher();
-    generateEvents(publisher);
-    var handler = eventSourceHandler(publisher);
-    handler(r);
-  });
+
+  app.get("/events", _handlerEventsWithoutUser);
+
+  app.get("/events/<user>", _handlerEventsWithtUser);
 
   io.serve(app, "localhost", 8080);
 }
 
-generateEvents(EventSourcePublisher publisher) {
+_handlerEventsWithoutUser(dynamic r) {
+  final publisher = EventSourcePublisher();
+  generateEvents(publisher);
+  var handler = eventSourceHandler(publisher);
+  handler(r);
+}
+
+_handlerEventsWithtUser(dynamic request, String user) {
+  final publisher = EventSourcePublisher();
+  generateEvents(publisher, user: user);
+  var handler = eventSourceHandler(publisher, channel: user);
+  handler(request);
+}
+
+generateEvents(
+  EventSourcePublisher publisher, {
+  String? user,
+}) {
   int id = 0;
-  Timer.periodic(const Duration(milliseconds: 300), (timer) {
+  Timer.periodic(const Duration(milliseconds: 500), (timer) {
     final data = json.encode({
-      'name': id,
+      'id': id,
       'message': 'event $id',
       'finished': id == 10,
+      'user': user,
     });
-    publisher.add(Event(data: data));
+    publisher.add(Event(data: data), channels: [user ?? '']);
 
-    if (id == 10) {
+    if (id == 100) {
       timer.cancel();
 
       publisher.close();
