d85be932dfe2fd841093781d2ee714f706696600
Eric Seidel <github@eseidel.com>
2022-11-14 13:41:53 -0800

    Update to modern Dart syntax and lints
    Also changed to use shelf_router instead of shelf_route since the latter is deprecated.

--- example/shelf_eventsource_example.dart
+++ example/shelf_eventsource_example.dart
@@ -1,7 +1,7 @@
 import "dart:async";
 
 import "package:shelf/shelf_io.dart" as io;
-import "package:shelf_route/shelf_route.dart" as routing;
+import "package:shelf_router/shelf_router.dart" as routing;
 
 import "package:eventsource/publisher.dart";
 import "package:shelf_eventsource/shelf_eventsource.dart";
@@ -9,7 +9,7 @@ import "package:shelf_eventsource/shelf_eventsource.dart";
 main() {
   // create the publisher object that will manage event publishing to
   // subscribers
-  EventSourcePublisher publisher = new EventSourcePublisher(cacheCapacity: 100);
+  EventSourcePublisher publisher = EventSourcePublisher(cacheCapacity: 100);
 
   // generate some dummy events
   generateEvents(publisher);
@@ -21,28 +21,27 @@ main() {
       eventSourceHandler(publisher, channel: "mychannel", gzip: true);
 
   // create a router to serve the different event sources
-  var router = routing.router();
+  var router = routing.Router();
   router.get("/events", handler);
   router.get("/mychannel", channelHandler);
 
   // serve localhost with our router
-  io.serve(router.handler, "localhost", 8080);
+  io.serve(router, "localhost", 8080);
 }
 
 generateEvents(publisher) {
   int id = 0;
-  new Timer.periodic(const Duration(seconds: 1), (timer) {
+  Timer.periodic(const Duration(seconds: 1), (timer) {
     // publish an event on the default channel
-    publisher
-        .add(new Event.message(id: "$id", data: "Always the same message?"));
+    publisher.add(Event.message(id: "$id", data: "Always the same message?"));
     // publish event in a channel
     publisher.add(
-        new Event.message(id: "$id", data: "Mychannel Message"), ["mychannel"]);
+        Event.message(id: "$id", data: "Mychannel Message"), ["mychannel"]);
     if (id == 25) {
       timer.cancel();
       // broadcast last message to both channels and close them
       publisher.add(
-          new Event(event: "goodbye", data: "Goodbye all!"), ["", "mychannel"]);
+          Event(event: "goodbye", data: "Goodbye all!"), ["", "mychannel"]);
       publisher.closeAll();
     }
   });
--- lib/shelf_eventsource.dart
+++ lib/shelf_eventsource.dart
@@ -1,28 +1,27 @@
 library shelf_eventsource;
 
-import "dart:convert";
+import 'dart:convert';
 
 import "package:eventsource/publisher.dart";
 import "package:eventsource/src/encoder.dart";
 import "package:shelf/shelf.dart";
-import 'package:stream_channel/stream_channel.dart';
 
 /// Create a shelf handler for the specified channel.
 /// This handler can be passed to the [shelf.serve] method.
 Handler eventSourceHandler(EventSourcePublisher publisher,
-    {String channel: "", bool gzip: false}) {
+    {String channel = "", bool gzip = false}) {
   // define the handler
   Response shelfHandler(Request request) {
     if (request.method != "GET") {
-      return new Response.notFound(null);
+      return Response.notFound(null);
     }
 
     if (request.headers["Accept"] != "text/event-stream") {
-      return new Response(400, body: "Bad Request");
+      return Response(400, body: "Bad Request");
     }
 
     if (!request.canHijack) {
-      throw new ArgumentError("eventSourceHandler may only be used with a "
+      throw ArgumentError("eventSourceHandler may only be used with a "
           "server that supports request hijacking.");
     }
 
@@ -32,10 +31,9 @@ Handler eventSourceHandler(EventSourcePublisher publisher,
 
     // hijack the raw underlying channel
     request.hijack((untypedChannel) {
-      var socketChannel =
-          (untypedChannel as StreamChannel).cast/*<List<int>>*/();
+      var socketChannel = (untypedChannel).cast<List<int>>();
       // create a regular UTF8 sink to write headers
-      var sink = UTF8.encoder.startChunkedConversion(socketChannel.sink);
+      var sink = utf8.encoder.startChunkedConversion(socketChannel.sink);
       // write headers
       sink.add("HTTP/1.1 200 OK\r\n"
           "Content-Type: text/event-stream; charset=utf-8\r\n"
@@ -45,9 +43,9 @@ Handler eventSourceHandler(EventSourcePublisher publisher,
       sink.add("\r\n");
 
       // create encoder for this connection
-      var encodedSink = new EventSourceEncoder(compressed: useGzip)
+      var encodedSink = EventSourceEncoder(compressed: useGzip)
           .startChunkedConversion(socketChannel.sink);
-      
+
       // initialize the new subscription
       publisher.newSubscription(
           onEvent: encodedSink.add,
@@ -58,8 +56,6 @@ Handler eventSourceHandler(EventSourcePublisher publisher,
 
     // [request.hijack] is guaranteed to throw a [HijackException], so we'll
     // never get here.
-    assert(false);
-    return null;
   }
 
   return shelfHandler;
